8566a66c4dae2f2dcf67775028359ea08ca21d4b
Wel15 <ffw150@163.com>
2025-01-06 07:34:54 -0500

    2516

--- src/fr_sp1.rs
+++ src/fr_sp1.rs
@@ -1,30 +1,61 @@
-use super::ff::*;
-use core::fmt::{self, Debug, Display};
-use core::iter::{Product, Sum};
-use core::ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign};
-use rand_core::RngCore;
+use crate::{
+    impl_add_binop_specify_output, impl_binops_additive_specify_output,
+    impl_binops_multiplicative_mixed, impl_sub_binop_specify_output, impl_sum_prod,
+};
+use core::ops::{Add, Mul, Neg, Sub};
+use ff::{PrimeField, FromUniformBytes};
+use rand::RngCore;
+use sp1_intrinsics::{
+    bn254::syscall_bn254_scalar_muladd,
+    memory::memcpy32,
+};
+use std::convert::TryInto;
+use std::io;
+use std::io::{Read, Write};
 use subtle::{Choice, ConditionallySelectable, ConstantTimeEq, CtOption};
 
+/// redirected to syscall_bn254_scalar_arith.
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[repr(transparent)]
+pub struct Fr([u32; 8]);
+
 const MODULUS: Fr = Fr([
-    0x43E1F593,
-    0x2833E848,
-    0x81585D2,
-    0xB85045B6,
-    0xE131A029,
-    0x30644E72,
-    0x0,
-    0x0,
+    0xf0000001, 0x43e1f593, 0x79b97091, 0x2833e848, 0x8181585d, 0xb85045b6, 0xe131a029, 0x30644e72,
+]);
+
+const MODULUS_STR: &str = "0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001";
+
+const GENERATOR: Fr = Fr([0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+
+const S: u32 = 28;
+
+const ROOT_OF_UNITY: Fr = Fr([
+    0xd34f1ed9, 0x60c37c9c, 0x3215cf6d, 0xd39329c8, 0x98865ea9, 0x3dd31f74, 0x03ddb9f5, 0x166d18b7,
+]);
+
+const TWO_INV: Fr = Fr([
+    0xa1f0fac9, 0xf8000001, 0x9419f424, 0x3cdcb848, 0xdc2822db, 0x40c0ac2e, 0x18322739, 0x7098d014,
+]);
+
+const ROOT_OF_UNITY_INV: Fr = Fr([
+    0x0ed3e50a, 0x414e6dba, 0xb22625f5, 0x9115aba7, 0x1bbe5871, 0x80f34361, 0x04812717, 0x4daabc26,
+]);
+
+const DELTA: Fr = Fr([
+    0x870e56bb, 0xe533e9a2, 0x5b5f898e, 0x5e963f25, 0x64ec26aa, 0xd4c86e71, 0x09226b6e, 0x22c6f0ca,
+]);
+
+const ZETA: Fr = Fr([
+    0x8b17ea66, 0xb99c90dd, 0x5bfc4108, 0x8d8daaa7, 0xb3c4d79d, 0x41a91758, 0x00, 0x00,
 ]);
 
 #[inline(always)]
-const fn sbb_u32(a: u32, b: u32, borrow: u32) -> (u32, u32) {
-    let ret = (a as u64).wrapping_sub((b as u64) + (borrow as u64));
+pub(crate) const fn sbb_u32(a: u32, b: u32, borrow: u32) -> (u32, u32) {
+    let ret = (a as u64).wrapping_sub((b as u64) + ((borrow >> 31) as u64));
     (ret as u32, (ret >> 32) as u32)
 }
 
-/// The BN254 scalar field Fr.
-#[derive(Copy, Clone, Default, PartialEq, Eq)]
-pub struct Fr(pub [u32; 8]);
+static ONE: Fr = Fr::one();
 
 impl Fr {
     #[inline]
@@ -82,182 +113,279 @@ impl Fr {
         32
     }
 
-    #[inline]
-    pub fn conditional_select(a: &Self, b: &Self, choice: Choice) -> Self {
-        let mut res = Self::zero();
-        for i in 0..8 {
-            res.0[i] = u32::conditional_select(&a.0[i], &b.0[i], choice);
-        }
-        res
-    }
-}
-
-impl Add<&Fr> for &Fr {
-    type Output = Fr;
-
-    #[inline]
-    fn add(self, rhs: &Fr) -> Fr {
-        let mut tmp = Fr::zero();
+    pub fn mul(&self, rhs: &Self) -> Fr {
+        let mut p = core::mem::MaybeUninit::<[u32; 8]>::uninit();
         unsafe {
-            sp1_intrinsics::bn254::syscall_bn254_scalar_muladd(
-                &mut tmp.0,
-                &Fr::one().0,
+            memcpy32(&self.0, p.as_mut_ptr());
+            syscall_bn254_scalar_muladd(
+                p.as_mut_ptr(),
                 &self.0,
                 &rhs.0,
+                &Self::zero().0
             );
+            Fr(p.assume_init())
         }
-        tmp
     }
-}
 
-impl Add<Fr> for Fr {
-    type Output = Fr;
-
-    #[inline]
-    fn add(self, rhs: Fr) -> Fr {
-        &self + &rhs
+    pub fn sub(&self, _rhs: &Self) -> Fr {
+        todo!()
     }
-}
 
-impl Add<&Fr> for Fr {
-    type Output = Fr;
-
-    #[inline]
-    fn add(self, rhs: &Fr) -> Fr {
-        &self + rhs
-    }
-}
-
-impl AddAssign<Fr> for Fr {
-    #[inline]
-    fn add_assign(&mut self, rhs: Fr) {
-        *self = &*self + &rhs;
-    }
-}
-
-impl AddAssign<&Fr> for Fr {
-    #[inline]
-    fn add_assign(&mut self, rhs: &Fr) {
-        *self = &*self + rhs;
-    }
-}
-
-impl Sub<&Fr> for &Fr {
-    type Output = Fr;
-
-    #[inline]
-    fn sub(self, rhs: &Fr) -> Fr {
-        let mut tmp = Fr::zero();
-        let neg_rhs = -rhs;
+    pub fn add(&self, rhs: &Self) -> Fr {
+        let mut p = core::mem::MaybeUninit::<Fr>::uninit();
         unsafe {
-            sp1_intrinsics::bn254::syscall_bn254_scalar_muladd(
-                &mut tmp.0,
-                &Fr::one().0,
-                &self.0,
-                &neg_rhs.0,
+            memcpy32(self, p.as_mut_ptr());
+            syscall_bn254_scalar_muladd(
+                p.as_mut_ptr(),
+                &rhs.0,
+                &ONE.0,
+                &self.0
             );
+            p.assume_init()
         }
-        tmp
     }
 }
 
-impl Sub<Fr> for Fr {
-    type Output = Fr;
+impl_binops_additive_specify_output!(Fr, Fr, Fr);
+impl_binops_multiplicative_mixed!(Fr, Fr, Fr);
+impl_sum_prod!(Fr);
 
-    #[inline]
-    fn sub(self, rhs: Fr) -> Fr {
-        &self - &rhs
-    }
-}
-
-impl Sub<&Fr> for Fr {
-    type Output = Fr;
-
-    #[inline]
-    fn sub(self, rhs: &Fr) -> Fr {
-        &self - rhs
-    }
-}
-
-impl SubAssign<Fr> for Fr {
+impl ::core::ops::SubAssign<Fr> for Fr {
     #[inline]
     fn sub_assign(&mut self, rhs: Fr) {
-        *self = &*self - &rhs;
+        self.sub_assign(&rhs);
     }
 }
 
-impl SubAssign<&Fr> for Fr {
+impl<'b> ::core::ops::SubAssign<&'b Fr> for Fr {
     #[inline]
-    fn sub_assign(&mut self, rhs: &Fr) {
+    fn sub_assign(&mut self, rhs: &'b Fr) {
         *self = &*self - rhs;
     }
 }
 
-impl Mul<&Fr> for &Fr {
-    type Output = Fr;
-
+impl ::core::ops::AddAssign<Fr> for Fr {
     #[inline]
-    fn mul(self, rhs: &Fr) -> Fr {
-        let mut tmp = Fr::zero();
+    fn add_assign(&mut self, rhs: Fr) {
+        self.add_assign(&rhs);
+    }
+}
+
+impl<'b> ::core::ops::AddAssign<&'b Fr> for Fr {
+    #[inline]
+    fn add_assign(&mut self, rhs: &'b Fr) {
         unsafe {
-            sp1_intrinsics::bn254::syscall_bn254_scalar_muladd(
-                &mut tmp.0,
-                &self.0,
+            syscall_bn254_scalar_muladd(
+                &mut self.0,
                 &rhs.0,
-                &Fr::zero().0,
+                &ONE.0,
+                &self.0
             );
         }
-        tmp
     }
 }
 
-impl Mul<Fr> for Fr {
-    type Output = Fr;
-
-    #[inline]
-    fn mul(self, rhs: Fr) -> Fr {
-        &self * &rhs
-    }
-}
-
-impl Mul<&Fr> for Fr {
-    type Output = Fr;
-
-    #[inline]
-    fn mul(self, rhs: &Fr) -> Fr {
-        &self * rhs
-    }
-}
-
-impl MulAssign<Fr> for Fr {
+impl core::ops::MulAssign<Fr> for Fr {
     #[inline]
     fn mul_assign(&mut self, rhs: Fr) {
-        *self = &*self * &rhs;
+        self.mul_assign(&rhs);
     }
 }
 
-impl MulAssign<&Fr> for Fr {
+impl<'b> core::ops::MulAssign<&'b Fr> for Fr {
     #[inline]
-    fn mul_assign(&mut self, rhs: &Fr) {
-        *self = &*self * rhs;
+    fn mul_assign(&mut self, rhs: &'b Fr) {
+        let tmp = *self;
+        unsafe {
+            syscall_bn254_scalar_muladd(
+                &mut self.0,
+                &tmp.0,
+                &rhs.0,
+                &Self::zero().0
+            );
+        }
     }
 }
 
-impl Neg for &Fr {
+impl ff::Field for Fr {
+    const ZERO: Self = Self::zero();
+    const ONE: Self = Self::one();
+
+    fn double(&self) -> Fr {
+        self + self
+    }
+
+    #[inline]
+    fn square(&self) -> Fr {
+        self * self
+    }
+
+    fn invert(&self) -> CtOption<Fr> {
+        todo!()
+    }
+
+    fn random(_rng: impl RngCore) -> Fr {
+        todo!()
+    }
+
+    fn sqrt_ratio(_num: &Self, _div: &Self) -> (Choice, Self) {
+        todo!()
+    }
+}
+
+impl ff::PrimeField for Fr {
+    type Repr = [u8; 32];
+
+    const NUM_BITS: u32 = 254;
+    const CAPACITY: u32 = 253;
+    const MODULUS: &'static str = MODULUS_STR;
+    const MULTIPLICATIVE_GENERATOR: Self = GENERATOR;
+    const ROOT_OF_UNITY: Self = ROOT_OF_UNITY;
+    const ROOT_OF_UNITY_INV: Self = ROOT_OF_UNITY_INV;
+    const TWO_INV: Self = TWO_INV;
+    const DELTA: Self = DELTA;
+    const S: u32 = S;
+
+    fn from_repr(repr: Self::Repr) -> CtOption<Self> {
+        Self::from_bytes(&repr)
+    }
+
+    fn to_repr(&self) -> Self::Repr {
+        let mut r = [0u8; 32];
+
+        r[0..4].copy_from_slice(&self.0[0].to_le_bytes());
+        r[4..8].copy_from_slice(&self.0[1].to_le_bytes());
+        r[8..12].copy_from_slice(&self.0[2].to_le_bytes());
+        r[12..16].copy_from_slice(&self.0[3].to_le_bytes());
+        r[16..20].copy_from_slice(&self.0[4].to_le_bytes());
+        r[20..24].copy_from_slice(&self.0[5].to_le_bytes());
+        r[24..28].copy_from_slice(&self.0[6].to_le_bytes());
+        r[28..32].copy_from_slice(&self.0[7].to_le_bytes());
+
+        r
+    }
+
+    fn from_u128(v: u128) -> Self {
+        let a0 = v as u32;
+        let a1 = (v >> 32) as u32;
+        let a2 = (v >> 64) as u32;
+        let a3 = (v >> 96) as u32;
+        let mut buf = [0u8; 32];
+
+        buf[0..4].copy_from_slice(&a0.to_le_bytes());
+        buf[4..8].copy_from_slice(&a1.to_le_bytes());
+        buf[8..12].copy_from_slice(&a2.to_le_bytes());
+        buf[12..16].copy_from_slice(&a3.to_le_bytes());
+        Self::from_repr_vartime(buf).unwrap()
+    }
+
+    fn is_odd(&self) -> Choice {
+        Choice::from((self.0[0] as u8) & 0x01_u8)
+    }
+}
+
+impl crate::serde::SerdeObject for Fr {
+    fn from_raw_bytes_unchecked(_bytes: &[u8]) -> Self {
+        todo!()
+    }
+
+    fn from_raw_bytes(_bytes: &[u8]) -> Option<Self> {
+        todo!()
+    }
+
+    fn to_raw_bytes(&self) -> Vec<u8> {
+        todo!()
+    }
+
+    fn read_raw_unchecked<R: Read>(_reader: &mut R) -> Self {
+        todo!()
+    }
+
+    fn read_raw<R: Read>(_reader: &mut R) -> io::Result<Self> {
+        todo!()
+    }
+
+    fn write_raw<W: Write>(&self, _writer: &mut W) -> io::Result<()> {
+        todo!()
+    }
+}
+
+impl From<u64> for Fr {
+    fn from(val: u64) -> Fr {
+        let v_lo = val as u32;
+        let v_hi = (val >> 32) as u32;
+        Fr([v_lo, v_hi, 0, 0, 0, 0, 0, 0])
+    }
+}
+
+impl FromUniformBytes<64> for Fr {
+    fn from_uniform_bytes(_bytes: &[u8; 64]) -> Self {
+        todo!()
+    }
+}
+
+impl ConstantTimeEq for Fr {
+    fn ct_eq(&self, other: &Self) -> Choice {
+        self.0[0].ct_eq(&other.0[0])
+            & self.0[1].ct_eq(&other.0[1])
+            & self.0[2].ct_eq(&other.0[2])
+            & self.0[3].ct_eq(&other.0[3])
+    }
+}
+
+impl core::cmp::Ord for Fr {
+    fn cmp(&self, other: &Self) -> core::cmp::Ordering {
+        let left = self.to_repr();
+        let right = other.to_repr();
+        left.iter()
+            .zip(right.iter())
+            .rev()
+            .find_map(|(left_byte, right_byte)| match left_byte.cmp(right_byte) {
+                core::cmp::Ordering::Equal => None,
+                res => Some(res),
+            })
+            .unwrap_or(core::cmp::Ordering::Equal)
+    }
+}
+
+impl core::cmp::PartialOrd for Fr {
+    fn partial_cmp(&self, other: &Self) -> Option<core::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl ConditionallySelectable for Fr {
+    fn conditional_select(a: &Self, b: &Self, choice: Choice) -> Self {
+        Fr([
+            u32::conditional_select(&a.0[0], &b.0[0], choice),
+            u32::conditional_select(&a.0[1], &b.0[1], choice),
+            u32::conditional_select(&a.0[2], &b.0[2], choice),
+            u32::conditional_select(&a.0[3], &b.0[3], choice),
+            u32::conditional_select(&a.0[4], &b.0[4], choice),
+            u32::conditional_select(&a.0[5], &b.0[5], choice),
+            u32::conditional_select(&a.0[6], &b.0[6], choice),
+            u32::conditional_select(&a.0[7], &b.0[7], choice),
+        ])
+    }
+}
+
+impl ff::WithSmallOrderMulGroup<3> for Fr {
+    const ZETA: Self = ZETA;
+}
+
+impl Default for Fr {
+    #[inline]
+    fn default() -> Self {
+        Self::zero()
+    }
+}
+
+impl<'a> Neg for &'a Fr {
     type Output = Fr;
 
     #[inline]
     fn neg(self) -> Fr {
-        let mut tmp = Fr::zero();
-        unsafe {
-            sp1_intrinsics::bn254::syscall_bn254_scalar_muladd(
-                &mut tmp.0,
-                &[0xFFFFFFFF; 8],
-                &self.0,
-                &Fr::zero().0,
-            );
-        }
-        tmp
+        unimplemented!()
     }
 }
 
@@ -266,212 +394,33 @@ impl Neg for Fr {
 
     #[inline]
     fn neg(self) -> Fr {
-        -&self
+        unimplemented!()
     }
 }
 
-impl Field for Fr {
-    const ZERO: Self = Self::zero();
-    const ONE: Self = Self::one();
+impl<'a, 'b> Add<&'b Fr> for &'a Fr {
+    type Output = Fr;
 
-    fn random(mut rng: impl RngCore) -> Self {
-        let mut buf = [0u8; 32];
-        loop {
-            rng.fill_bytes(&mut buf);
-            if let Some(fr) = Self::from_bytes(&buf).into() {
-                return fr;
-            }
-        }
-    }
-
-    fn square(&self) -> Self {
-        let mut tmp = Self::zero();
-        unsafe {
-            sp1_intrinsics::bn254::syscall_bn254_scalar_muladd(
-                &mut tmp.0,
-                &self.0,
-                &self.0,
-                &Self::zero().0,
-            );
-        }
-        tmp
-    }
-
-    fn double(&self) -> Self {
-        let mut tmp = Self::zero();
-        unsafe {
-            sp1_intrinsics::bn254::syscall_bn254_scalar_muladd(
-                &mut tmp.0,
-                &[2, 0, 0, 0, 0, 0, 0, 0],
-                &self.0,
-                &Self::zero().0,
-            );
-        }
-        tmp
-    }
-
-    fn invert(&self) -> CtOption<Self> {
-        let mut tmp = Self::zero();
-        unsafe {
-            sp1_intrinsics::bn254::syscall_bn254_scalar_muladd(
-                &mut tmp.0,
-                &[0xFFFFFFFF; 8],
-                &self.0,
-                &Self::zero().0,
-            );
-        }
-        CtOption::new(tmp, !self.ct_eq(&Self::zero()))
-    }
-
-    fn sqrt_ratio(_: &Self, _: &Self) -> (Choice, Self) {
-        (Choice::from(1u8), Self::one())
+    #[inline]
+    fn add(self, rhs: &'b Fr) -> Fr {
+        self.add(rhs)
     }
 }
 
-impl PrimeField for Fr {
-    type Repr = [u8; 32];
+impl<'a, 'b> Sub<&'b Fr> for &'a Fr {
+    type Output = Fr;
 
-    const MODULUS: &'static str = "0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001";
-    const NUM_BITS: u32 = 254;
-    const CAPACITY: u32 = 253;
-    
-    const TWO_INV: Self = Fr::from_raw([2, 0, 0, 0]);
-    const MULTIPLICATIVE_GENERATOR: Self = Fr::from_raw([2, 0, 0, 0]);
-    
-    const S: u32 = 28;
-    
-    const ROOT_OF_UNITY: Self = Fr::from_raw([
-        0xd35d438dc58f0d9d,
-        0x0a78eb28f5c70b3d,
-        0x666ea36f7879462c,
-        0x0e0a77c19a07df2f,
-    ]);
-    
-    const ROOT_OF_UNITY_INV: Self = Fr::from_raw([
-        0x4dca135978a8016a,
-        0x0559f96e8c0d3308,
-        0xc6695f92b50a8313,
-        0x12c1b6aaf3dd5b7b,
-    ]);
-    
-    const DELTA: Self = Fr::from_raw([
-        0x94575516c2c9b3b2,
-        0x00000001236392ee,
-        0x0000000000000001,
-        0x0000000000000000,
-    ]);
-
-    fn from_repr(repr: Self::Repr) -> CtOption<Self> {
-        Self::from_bytes(&repr)
-    }
-
-    fn to_repr(&self) -> Self::Repr {
-        let mut res = [0u8; 32];
-        for i in 0..8 {
-            res[i*4..(i+1)*4].copy_from_slice(&self.0[i].to_le_bytes());
-        }
-        res
-    }
-
-    fn is_odd(&self) -> Choice {
-        Choice::from((self.0[0] & 1) as u8)
+    #[inline]
+    fn sub(self, rhs: &'b Fr) -> Fr {
+        self.sub(rhs)
     }
 }
 
-impl ConditionallySelectable for Fr {
-    fn conditional_select(a: &Self, b: &Self, choice: Choice) -> Self {
-        Fr::conditional_select(a, b, choice)
-    }
-}
+impl<'a, 'b> Mul<&'b Fr> for &'a Fr {
+    type Output = Fr;
 
-impl ConstantTimeEq for Fr {
-    fn ct_eq(&self, other: &Self) -> Choice {
-        let mut tmp = 0u8;
-        for i in 0..8 {
-            tmp |= (self.0[i] ^ other.0[i]) as u8;
-        }
-        Choice::from((tmp == 0) as u8)
-    }
-}
-
-impl Sum for Fr {
-    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
-        iter.fold(Self::zero(), Add::add)
-    }
-}
-
-impl<'a> Sum<&'a Fr> for Fr {
-    fn sum<I: Iterator<Item = &'a Fr>>(iter: I) -> Self {
-        iter.fold(Self::zero(), Add::add)
-    }
-}
-
-impl Product for Fr {
-    fn product<I: Iterator<Item = Self>>(iter: I) -> Self {
-        iter.fold(Self::one(), Mul::mul)
-    }
-}
-
-impl<'a> Product<&'a Fr> for Fr {
-    fn product<I: Iterator<Item = &'a Fr>>(iter: I) -> Self {
-        iter.fold(Self::one(), Mul::mul)
-    }
-}
-
-impl Debug for Fr {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Fr([{:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}])",
-            self.0[0], self.0[1], self.0[2], self.0[3],
-            self.0[4], self.0[5], self.0[6], self.0[7])
-    }
-}
-
-impl Display for Fr {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-impl From<u64> for Fr {
-    fn from(val: u64) -> Self {
-        let mut tmp = [0u32; 8];
-        tmp[0] = val as u32;
-        tmp[1] = (val >> 32) as u32;
-        Fr(tmp)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_fr_from_raw() {
-        let raw = [1u64, 2u64, 3u64, 4u64];
-        let fr = Fr::from_raw(raw);
-        
-        assert_eq!(fr.0[0], 1u32);
-        assert_eq!(fr.0[1], 0u32);
-        assert_eq!(fr.0[2], 2u32);
-        assert_eq!(fr.0[3], 0u32);
-        assert_eq!(fr.0[4], 3u32);
-        assert_eq!(fr.0[5], 0u32);
-        assert_eq!(fr.0[6], 4u32);
-        assert_eq!(fr.0[7], 0u32);
-    }
-
-    #[test]
-    fn test_fr_basic_ops() {
-        let a = Fr::from_raw([1, 0, 0, 0]);
-        let b = Fr::from_raw([2, 0, 0, 0]);
-        
-        let c = &a + &b;
-        assert_eq!(c.0[0], 3);
-        
-        let d = &c - &b;
-        assert_eq!(d.0[0], 1);
-        
-        let e = &a * &b;
-        assert_eq!(e.0[0], 2);
+    #[inline]
+    fn mul(self, rhs: &'b Fr) -> Fr {
+        self.mul(rhs)
     }
 }
